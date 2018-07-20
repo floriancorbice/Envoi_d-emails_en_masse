461b9e670fee0eea92f77e129cc75e63cfc3d9a7
floriancorbice <40582242+floriancorbice@users.noreply.github.com>
2018-07-20 09:32:55 +0200

    Delete testcsv.rb

--- testcsv.rb
+++
@@ -1,8 +0,0 @@
-require 'csv'
-
-csv = CSV.read('/home/luxin/Bureau/THP/Semaine_3/Mercredi/test.csv', :headers=>true)
-p csv['name'] #=>["Raja", "Mathew", "harin", "Soumi"]
-
-#or even:
-t = CSV.table('/home/luxin/Bureau/THP/Semaine_3/Mercredi/test.csv')
-p t[:no1] #=> [77489, 77559, 77787, 77251]
